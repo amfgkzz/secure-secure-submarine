003eccb507f702d80eb3233f28f4cffdaed0a151
Koua Xiong <kxiong517@gmail.com>
2019-06-26 12:58:31 -0500

    finished base

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -5,8 +5,12 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
+    pool.query(`SELECT "content" FROM "secret"
+    FULL OUTER JOIN "user" ON "secrecy_level"<"clearance_level"
+    WHERE "user"."id"=$1;`, [req.user.id])
+        .then(results => {
+            console.log('results.rows:',results.rows);
+            res.send(results.rows)})
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
