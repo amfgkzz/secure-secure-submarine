2dd14294015f3a883fc451d51fe8c247b4e3e431
Koua Xiong <kxiong517@gmail.com>
2019-06-26 13:42:11 -0500

    ready to deploy?

---
+++ build/precache-manifest.66ecc94916a3c495437e80170d890bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dcc2e9f5459a4b1d4952b35eb5b8485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c511f3e88ccbc276461c",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "a9791c0e3a1dc88ee73c",
+    "url": "/static/js/2.334b9d6a.chunk.js"
+  },
+  {
+    "revision": "c511f3e88ccbc276461c",
+    "url": "/static/js/main.82ab5e02.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
